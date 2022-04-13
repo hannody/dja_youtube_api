40b5299750d75e690eb124d3c0ef9b31e464f76e
Mohanad Abu Nayla <mohanad.abu-nayla@equatd.com>
2022-04-13 09:00:30 +0800

    urls for add_search view

--- video_tracker/app/urls.py
+++ video_tracker/app/urls.py
@@ -1,7 +1,8 @@
-from .views import index, channel_search
+from .views import (index, channel_search, add_channel)
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
     path('channelsearch/', channel_search, name='channel-search'),
+    path('addchannel/<channel_id>/', add_channel, name='add-channel'),
 ]
--- video_tracker/app/views.py
+++ video_tracker/app/views.py
@@ -1,7 +1,13 @@
+# Django
 from django.shortcuts import render
-from .models import Channel, Video
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+
+# Support libraries
 import requests
+
+# Local - Django
+from .models import Channel
 
 
 def index(request):
@@ -19,8 +25,9 @@ def channel_search(request):
     context = {'results': results}
     return render(request, 'partials/channel_search_results.html', context)
 
-
+@csrf_exempt
 def add_channel(request, channel_id):
+    url = f'https://www.googleapis.com/youtube/v3/channels?id={channel_id}&part=snippet,contentDetails&key={settings.YOUTUBE_API_KEY}'
     res = requests.get(url)
     result = res.json()['items'][0]
 
@@ -36,3 +43,7 @@ def add_channel(request, channel_id):
     channels = Channel.objects.all()
     context = {'channels': channels}
     return render(request, 'partials/channels.html', context)
+
+
+# https://www.youtube.com/watch?v=vukzYCi2_yE&t=8s
+# 35:43
